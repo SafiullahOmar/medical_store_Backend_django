8d2b6ddfa17339f900f5c40695668a19d43e0058
safiullah omar <omar.safiullah@gmail.com>
2024-10-18 03:08:52 -0500

    admin url

--- medicalapp/admin.py
+++ medicalapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Company,MedicalDetails,Medicine,Employee,Customer,Bill,EmployeeSalary,BillDetails,CustomerRequest,CompanyAccount,CompanyBank,EmployeeBank
 # Register your models here.
+
+admin.site.register(Company)
+admin.site.register(MedicalDetails)
+admin.site.register(Medicine)
+admin.site.register(Employee)
+admin.site.register(Bill)
+admin.site.register(Customer)
+admin.site.register(EmployeeSalary)
+admin.site.register(EmployeeBank)
+admin.site.register(BillDetails)
+admin.site.register(CustomerRequest)
+admin.site.register(CompanyAccount)
+admin.site.register(CompanyBank)
+
+
+
